72167d83ef7e28b546e25dbd52d096391f20681b
Dharshini-457 <dharshinir619@gmail.com>
2026-09-15 16:19:51 +0530

    first commit

---
+++ class/Non_Repeat.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+
+
+public class Non_Repeat {
+    public static void main(String[] args) {
+        
+    // code here
+        int[] arr = {1,5,6,18,20}
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        
+        for(int n : arr){
+            freq.put(n , freq.getOrDefault(n , 0) + 1);
+        }
+        for(int i : arr){
+            if(freq.get(i) == 1){
+                System.out.println(i);
+            }
+        }
+        System.out.println(0);
+    }
+}
